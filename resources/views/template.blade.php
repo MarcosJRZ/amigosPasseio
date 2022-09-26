b6b8074dedb2e4e64f15a362692d9645e3f4e509
Marcos JR <j_union_ho@hotmail.com>
2022-09-26 10:12:36 -0300

    ajuste projeto base

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -15,29 +15,31 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg bg-dark ">
-        <a class="navbar-brand ps-3" href="#">Logo</a>
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/itens">Buscar item</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/recursos">Recursos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/receitas">Receitas</a>
-                    </li>
-                </ul>
+    @if (!isset($nav_off))
+        <nav class="navbar navbar-dark navbar-expand-lg bg-dark ">
+            <a class="navbar-brand ps-3" href="#">Logo</a>
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Buscar item</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Recursos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Receitas</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    @endif
     @yield('conteudo')
 </body>
 
